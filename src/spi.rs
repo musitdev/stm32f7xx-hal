c3cc71f6a1ff356e442969d889bf3d3b598c87a8
Hanno Braun <hanno@braun-embedded.com>
2019-06-03 12:24:48 +0200

    Suppress unused variable warning
    It seems the code using it was commented out. I think it makes sense to
    keep it around for now, until we've figured out what's going to happen
    with that code.

--- src/spi.rs
+++ src/spi.rs
@@ -290,7 +290,7 @@ macro_rules! hal {
                     // disable SS output
                     spi.cr2.write(|w| w.ssoe().clear_bit());
 
-                    let br = match clocks.$pclkX().0 / freq.0 {
+                    let _br = match clocks.$pclkX().0 / freq.0 {
                         0 => unreachable!(),
                         1...2 => 0b000,
                         3...5 => 0b001,
